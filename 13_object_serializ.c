fcf99e9edd301a665702e9a3cbe306f6427b7db7
afterloe <afterloe@foxmail.com>
2022-04-01 16:16:49 +0800

    feat: 写文件

--- 13_object_serializ.c
+++ 13_object_serializ.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Student {
+struct Student
+{
     int id;
     char name[32];
 };
@@ -9,9 +10,22 @@ struct Student {
 int main()
 {
     struct Student students[3] = {{1101, "afterloe"}, {1102, "joe"}, {1102, "Grace"}};
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         printf("%d -> %s\n", students[i].id, students[i].name);
     }
 
-    return -1;
+    FILE *f = fopen("a.txt", "w");
+
+    if (NULL == f)
+    {
+        perror("");
+        return -1;
+    }
+
+    fwrite(students, sizeof(students), 3, f);
+    printf("success.\n");
+    fclose(f);
+
+    return 0;
 }

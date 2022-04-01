cd02fd51909d83a137c975fee6c587d36f7e0def
afterloe <afterloe@foxmail.com>
2022-04-01 16:32:55 +0800

    feat: 数据序列化与读入写出

--- 13_object_serializ.c
+++ 13_object_serializ.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Student
+typedef struct Student
 {
     int id;
     char name[32];
-};
+} Student;
+
+extern void obj2file(const char *);
+extern void file2obj(const char *);
 
 int main()
 {
-    struct Student students[3] = {{1101, "afterloe"}, {1102, "joe"}, {1102, "Grace"}};
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%d -> %s\n", students[i].id, students[i].name);
-    }
+    // obj2file("a.txt");
+    file2obj("a.txt");
+    return 0;
+}
 
-    FILE *f = fopen("a.txt", "w");
+void file2obj(const char *file_name)
+{
+    Student serialze[3];
+    FILE *f = fopen(file_name, "r");
+    if (NULL == f)
+    {
+        perror("");
+        return;
+    }
+    int count = fread(serialze, sizeof(Student), 3, f);
+    printf("read %d object from file\n", count);
+    for (int i=0; i< 3; i++){
+        printf("%d -> %s\n", serialze[i].id, serialze[i].name);
+    }
+    return;
+}
+
+void obj2file(const char *file_name)
+{
+    Student students[3] = {{1101, "afterloe"}, {1102, "joe"}, {1102, "Grace"}};
+    FILE *f = fopen(file_name, "w");
 
     if (NULL == f)
     {
         perror("");
-        return -1;
+        return;
     }
 
     fwrite(students, sizeof(students), 3, f);
     printf("success.\n");
     fclose(f);
-
-    return 0;
 }

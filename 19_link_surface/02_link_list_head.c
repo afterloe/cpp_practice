00040f2dc583cd0ed846bda182e7a602f52f81eb
afterloe <605728727@qq.com>
2022-04-09 23:02:50 +0800

    删除链表节点

--- 19_link_surface/02_link_list_head.c
+++ 19_link_surface/02_link_list_head.c
@@ -8,46 +8,76 @@ typedef struct _link_node
     struct _link_node *next;
 } NODE;
 
-extern void init_linklist(struct _link_node **);
-extern void push(struct _link_node **, int);
-extern void insert(struct _link_node **, int, int);
+extern struct _link_node *init_linklist();
+extern void push(struct _link_node *, int);
+extern void insert(struct _link_node *, int, int);
 extern void printf_link_list(struct _link_node *);
+extern void del(struct _link_node *, int);
 
 int main()
 {
-    NODE *current = NULL;
-    init_linklist(&current);
-    srand(time(NULL));
+    NODE *current = init_linklist();
+
     // 连续尾插
+    // srand(time(NULL));
     // for (int i = 0; i < 10; i++)
     // {
     //     int num = rand() % 100;
     //     push(&current, num);
     // }
-    push(&current, 10);
-    push(&current, 20);
-    push(&current, 30);
+
+    push(current, 10);
+    push(current, 20);
+    push(current, 30);
     printf("指定插入前: \n");
     printf_link_list(current);
-    insert(&current, 20, 200);
-    insert(&current, 21, 1030);
+    insert(current, 20, 200);
+    insert(current, 21, 1030);
     printf("指定插入后: \n");
     printf_link_list(current);
 
+    printf("删除 200 ... ... \n");
+    del(current, 200);
+    printf("删除 200 后 \n");
+    printf_link_list(current);
     return EXIT_FAILURE;
 }
 
-void init_linklist(struct _link_node **tail_ptr)
+void del(struct _link_node *ptr, int value)
+{
+    NODE *prev = ptr;
+    NODE *currect = ptr->next;
+    while (currect != NULL)
+    {
+        if (value == currect->num)
+        {
+            break;
+        }
+        prev = currect;
+        currect = currect->next;
+    }
+
+    if (currect == NULL)
+    {
+        return;
+    }
+
+    prev->next = currect->next;
+    free(currect);
+    currect = NULL;
+}
+
+struct _link_node *init_linklist()
 {
     NODE *header = (NODE *)malloc(sizeof(struct _link_node));
     header->next = NULL;
-    *tail_ptr = header;
+    return header;
 }
 
-void insert(struct _link_node **header, int old_value, int new_value)
+void insert(struct _link_node *header, int old_value, int new_value)
 {
-    NODE *prev = *header;
-    NODE *current = (*header)->next;
+    NODE *prev = header;
+    NODE *current = header->next;
     while (current != NULL)
     {
         if (old_value == current->num)
@@ -66,10 +96,10 @@ void insert(struct _link_node **header, int old_value, int new_value)
     prev->next = new_node;
 }
 
-void push(struct _link_node **header, int value)
+void push(struct _link_node *header, int value)
 {
     // 使用尾部指针 实现尾插；
-    NODE *tail_ptr = *header;
+    NODE *tail_ptr = header;
     while (NULL != tail_ptr->next)
     {
         tail_ptr = tail_ptr->next;

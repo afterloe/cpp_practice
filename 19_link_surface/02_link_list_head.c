e3aec2e09db22bdf9b279b59385529edab6064f1
afterloe <605728727@qq.com>
2022-04-09 22:24:21 +0800

    feat: 指定位置插入

--- 19_link_surface/02_link_list_head.c
+++ 19_link_surface/02_link_list_head.c
@@ -9,7 +9,8 @@ typedef struct _link_node
 } NODE;
 
 extern void init_linklist(struct _link_node **);
-extern void insert_member(struct _link_node **, int);
+extern void push(struct _link_node **, int);
+extern void insert(struct _link_node **, int, int);
 extern void printf_link_list(struct _link_node *);
 
 int main()
@@ -17,12 +18,22 @@ int main()
     NODE *current = NULL;
     init_linklist(&current);
     srand(time(NULL));
-    for (int i = 0; i < 10; i++)
-    {
-        int num = rand() % 100;
-        insert_member(&current, num);
-    }
+    // 连续尾插
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     int num = rand() % 100;
+    //     push(&current, num);
+    // }
+    push(&current, 10);
+    push(&current, 20);
+    push(&current, 30);
+    printf("指定插入前: \n");
     printf_link_list(current);
+    insert(&current, 20, 200);
+    insert(&current, 21, 1030);
+    printf("指定插入后: \n");
+    printf_link_list(current);
+
     return EXIT_FAILURE;
 }
 
@@ -33,7 +44,29 @@ void init_linklist(struct _link_node **tail_ptr)
     *tail_ptr = header;
 }
 
-void insert_member(struct _link_node **header, int value)
+void insert(struct _link_node **header, int old_value, int new_value)
+{
+    NODE *prev = *header;
+    NODE *current = (*header)->next;
+    while (current != NULL)
+    {
+        if (old_value == current->num)
+        {
+            break;
+        }
+        prev = current;
+        current = current->next;
+    }
+
+    NODE *new_node = (NODE *)malloc(sizeof(struct _link_node));
+    new_node->num = new_value;
+    new_node->next = NULL;
+
+    new_node->next = current;
+    prev->next = new_node;
+}
+
+void push(struct _link_node **header, int value)
 {
     // 使用尾部指针 实现尾插；
     NODE *tail_ptr = *header;

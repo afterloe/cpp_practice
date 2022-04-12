7aeb696819f82f3b59fd6eb1af623cda24194289
afterloe <605728727@qq.com>
2022-04-12 22:57:51 +0800

    feat: 企业级链表数据的插入与遍历

--- 23_link_node_e/test_link_node.c
+++ 23_link_node_e/test_link_node.c
@@ -19,12 +19,38 @@ typedef void *LinkedList;
 
 extern LinkedList init();
 extern void insert(LinkedList, void *, int);
+extern void info(LinkedList, void (*)(void *));
 
 #endif
+
+typedef struct
+{
+    void *node;
+    char name[32];
+    int age;
+} Person;
+
+void print_person(void *data)
+{
+    Person *p = data;
+    printf("%s - %d | ", p->name, p->age);
+}
 
 int main()
 {
     LinkedList list = init();
+    Person p1 = {NULL, "aaa", 10};
+    Person p2 = {NULL, "bbb", 20};
+    Person p3 = {NULL, "ccc", 30};
+    Person p4 = {NULL, "ddd", 40};
+
+    insert(list, &p1, -1);
+    insert(list, &p2, -1);
+    insert(list, &p3, 0);
+    insert(list, &p4, -1);
+
+    info(list, print_person);
+
     return EXIT_SUCCESS;
 }
 
@@ -37,20 +63,43 @@ LinkedList init()
     return ptr;
 }
 
-void insert(LinkedList ptr, void * data, int pos)
+void insert(LinkedList ptr, void *data, int pos)
 {
     if (NULL == ptr)
     {
         return;
     }
-    struct linked_list * list = ptr;
+    struct linked_list *list = ptr;
     if (pos < 0 || pos > list->size)
     {
         pos = list->size;
     }
-    struct linked_node * current = list->header.next;
+    struct linked_node *node = data;
+    struct linked_node *current = &list->header;
     for (int idx = 0; idx < pos; idx++)
     {
         current = current->next;
     }
+
+    node->next = current->next;
+    current->next = node;
+
+    list->size++;
+}
+
+void info(LinkedList ptr, void (*print_fun)(void *))
+{
+    if (NULL == ptr)
+    {
+        return;
+    }
+    struct linked_list *list = ptr;
+    struct linked_node *current = &list->header;
+    printf("\nsize :: %d [", list->size);
+    for (int idx = 0; idx < list->size; idx++)
+    {
+        current = current->next;
+        print_fun(current);
+    }
+    printf("]\n");
 }
